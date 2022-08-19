f674eec837ab2300ea89b629d3cb312273ef9d4d
alexanderism55 <99040483+alexanderism55@users.noreply.github.com>
2022-08-19 14:09:14 +0200

    added the stop button

--- src/init.js
+++ src/init.js
@@ -1,5 +1,8 @@
 import { BEGIN_EVENT } from "../../data/constants.js";
+import { STOP_COUNT } from "../data/constants.js";
 import { START_COUNT } from "../../data/constants.js";
 import { startCountListener } from "./listeners/start-listener.js";
+import {stopCountListener } from "./listeners/stop-listener.js"
 
 startCountListener(document.getElementById(START_COUNT));
+stopCountListener(document.getElementById(STOP_COUNT));
